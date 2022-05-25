c572a50607e7e649a2e8688bf2e2ef3c65008d98
AnthonyRansom <100229413+AnthonyRansom@users.noreply.github.com>
2022-05-25 09:16:45 -0400

    exercise 13.5.5

--- Mapping_GeoJSON_linestrings/static/js/logic.js
+++ Mapping_GeoJSON_linestrings/static/js/logic.js
@@ -32,12 +32,22 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-"https://raw.githubusercontent.com/anthonyransom/Mapping_Earthquakes/Mapping_GeoJSON_linestrings/torontoRoutes.json"
 let torontoData = "https://anthonyransom.github.io/Mapping_Earthquakes/Mapping_GeoJSON_linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " + feature.properties.dst + "</h3>");
+      }
+  }).addTo(map);
 });
